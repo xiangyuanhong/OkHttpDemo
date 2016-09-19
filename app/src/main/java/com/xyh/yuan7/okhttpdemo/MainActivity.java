e3a7b26e307bbbd71f748fc92e2f05778b90ba34
xyh <1293604244@qq.com>
2016-09-20 00:50:21 +0800

    Initial commit

---
+++ app/src/main/java/com/xyh/yuan7/okhttpdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.xyh.yuan7.okhttpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private String URL="http://capi.douyucdn.cn/api/v1/live?limit=20&offset=0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OkHttpClient client = new OkHttpClient();
+
+
+
+
+    }
+
+    private void get() {
+        OkHttpManager.getAsync(URL, new OkHttpManager.DataCallBack() {
+            @Override
+            public void requestFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void requestSuccess(String result) throws Exception {
+                TextView textView= (TextView) findViewById(R.id.textview);
+                textView.setText(result);
+                         Log.i("TAG",result);
+            }
+        });
+    }
+}
